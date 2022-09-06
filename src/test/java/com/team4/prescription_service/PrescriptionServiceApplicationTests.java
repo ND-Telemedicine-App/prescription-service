ea5bc23cf47561324db6222f277671e707582f76
doanyennhi <s3880599@rmit.edu.vn>
2022-09-06 19:24:49 +1000

    remove @SpringBootTest

--- src/test/java/com/team4/prescription_service/PrescriptionServiceApplicationTests.java
+++ src/test/java/com/team4/prescription_service/PrescriptionServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.team4.prescription_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PrescriptionServiceApplicationTests {
 
     @Test
