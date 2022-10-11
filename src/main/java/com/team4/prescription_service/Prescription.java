69bd87146736008a01c0a48e20916dd9492d1949
doanyennhi <s3880599@rmit.edu.vn>
2022-10-12 04:01:13 +1100

    Change id generation type

--- src/main/java/com/team4/prescription_service/Prescription.java
+++ src/main/java/com/team4/prescription_service/Prescription.java
@@ -15,7 +15,7 @@ public class Prescription {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     //user's id is stored when making a prescription
